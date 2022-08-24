f2704a972c07d2f3fba3e0738bac8cf1879721dc
ngustavin <gust4f3rn@gmail.com>
2022-08-23 23:23:24 -0300

    :sparkles: feat: creating fake event producer

---
+++ A005(Tavinho)/__main__.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+from fake_web_events import Simulation
+
+client = boto3.client('firehose', region_name='us-east-1')
+
+def put_record(event: dict):
+    data = json.dumps(event) +  "\n"
+    response = client.put_record(
+        DeliveryStreamName='PUT-S3-3TJBe',
+        Record={"Data": data}
+    )
+    print(event)
+    return response
+
+simulation = Simulation(user_pool_size=100, sessions_per_day=100000)
+events = simulation.run(duration_seconds=3000)
+
+for event in events:
+    put_record(event)
